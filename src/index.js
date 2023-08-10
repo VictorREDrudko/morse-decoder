030b94694cea41cd301b97c8cec229004d2bd753
Viktor Rudko <viktor_RED_rudko@mail.ru>
2023-08-10 15:09:43 +0300

    docs: solve problems

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprArray = expr.split()
+    
+    let breakdownByTen = exprArray.map(el => {
+        const test = s => s.match(/..........?/g).map(v => v.length == 1 ? v + '_' : v)
+        return test(el)  
+    })
+
+    let numbersArray = breakdownByTen.flat()
+      
+    let substitutionForSymbol = numbersArray.map(pos => {
+        if(pos === '**********') {return ' '}
+        else {
+            let replacementNumber = pos.replaceAll(/11/gi, '-').replaceAll(/10/gi, '.').replaceAll(/0/gi, '')
+            return replacementNumber
+        }
+    })
+    
+    const dash = Object.keys(MORSE_TABLE)
+    const letter = Object.values(MORSE_TABLE)
+
+    let decoderWords = substitutionForSymbol.map((el, index) => {
+        if(el !== ' ') {
+                let indexDash = dash.indexOf(el)
+                let indexLetter = letter[indexDash]
+                return indexLetter
+        } else {return ' '}
+    })
+
+    const words = decoderWords.join().replaceAll(/,/gi, '')
+    
+    return words
 }
 
 module.exports = {
